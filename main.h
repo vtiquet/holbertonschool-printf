d87df55deae258efffba3102e6b8fc3cd079b1e5
vtiquet <valentin.tiquet@gmail.com>
2025-07-21 14:30:07 +0200

    Created files

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _putchar(char c)
+int _printf(const char *format, ...);
+
+#endif /*MAIN_H*/
